1d33329dc14261173258192b4d72154f93e00b68
Aaron <aaron.hsu86+github@gmail.com>
2023-09-18 11:52:20 -0700

    temp update

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const port = process.env.PORT;
 
 app.use(cors(),express.json(),express.urlencoded({ extended: true }));
 
-require('./routes/notes.routes')(app);
+//! Update routes name
+// require('./routes/cars.routes')(app);
 
 app.listen(port, () => console.log(`Listening on port: ${port}`) );
