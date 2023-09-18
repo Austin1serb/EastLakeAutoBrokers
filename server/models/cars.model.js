363779ef586aad30b4a49b1afdb2b32217072b53
Aaron <aaron.hsu86+github@gmail.com>
2023-09-18 15:15:31 -0700

    update models

--- server/models/cars.model.js
+++ server/models/cars.model.js
@@ -1,47 +1,75 @@
 const mongoose = require('mongoose')
 const CarsSchema = new mongoose.Schema({
     name: {
-        
+        type: String,
+        minlength: [2, "Name must be at least 2 characters long"]
     },
-    condition: {
-
+    conditionNew: {
+        type: Boolean,
+        default: true
     },
     year: {
-
+        type: Number,
+        minlength: [4, "Please provide a valid 4 digit year."], 
+        maxlength: [4, "Please provide a valid 4 digit year."]
     },
     make: {
-
+        type: String,
+        minlength: [2, "Make must be at least 2 characters long"]
     },
     trim: {
-
+        type: String,
+        minlength: [2, "Trim must be at least 2 characters long"]
     },
     color: {
-
+        type: String,
+        minlength: [2, "Color must be at least 2 characters long"]
     },
     interior: {
-
+        type: String,
+        minlength: [2, "Interior must be at least 2 characters long"]
     },
     interiorColor: {
-
+        type: String,
+        minlength: [2, "Interior Color must be at least 2 characters long"]
     },
     bodyType: {
-
+        type: String,
+        minlength: [2, "Body Type must be at least 2 characters long"]
     },
     fuelType: {
-
+        type: String,
+        minlength: [2, "Fuel Type must be at least 2 characters long"]
     },
     cylinders: {
-
+        type: String,
+        minlength: [2, "Cylinder info must be at least 2 characters long"]
     },
     driveTrain: {
-
+        type: String,
+        minlength: [2, "Drive Train info must be at least 2 characters long"]
     },
     transmission: {
-
+        type: String,
+        minlength: [2, "Transmission type must be at least 2 characters long"]
     },
     vin: {
-
+        type: Number,
+        min: [0, "Number cannot be negative"], 
+        required: [true, "VIN number is required"]
     },
+    engine: {
+        type: String,
+        minlength: [2, "Engine info must be at least 2 characters long"]
+    },
+    mileage: {
+        type: Number,
+        min: [0, "Number cannot be negative."]
+    },
+    stockNumber: {
+        type: Number,
+        min: [0, "Number cannot be negative."]
+    }
 
 }, {timestamps: true})
 const Cars = mongoose.model("Cars", CarsSchema)
---
+++ server/models/staffers.model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const StaffersSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: [true, "First name is required"],
+        minlength: [2, "First name must be at least 2 characters long"]
+    },
+    middleName: {
+        type: String
+    },
+    lastName: {
+        type: String,
+        required: [true, "Last name is required"],
+        minlength: [2, "Last name must be at least 2 characters long"]
+    },
+    position: {
+        type: String,
+        required: [true, "Position is required"],
+        minlength: [2, "Position must be at least 2 characters long"]
+    },
+    phoneNumber: {
+        type: Number,
+        required: [true, "Please provide a phone number"],
+        match: [/^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/, "Please provide a valid phone number"]
+    },
+    email: {
+        type: String,
+        required: [validateEmail, 'Please fill a valid email address']
+    },
+    bio: {
+        type: String,
+        maxlength: [256, "Please keep your description to under 256 characters."]
+    }
+}, {timestamps: true})
+const Staffers = mongoose.model("Staffers", StaffersSchema)
+module.exports = Staffers;
